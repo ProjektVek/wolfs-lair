abfd657080c6ad2dceb69aa0d30285061cef8224
Victor Hugo "VeK" Ferreira <ProjektVek@gmail.com>
2022-01-29 21:03:15 -0300

    added random number list
    added random number list

--- Scenes/LabelEquation.gd
+++ Scenes/LabelEquation.gd
@@ -5,17 +5,33 @@ var PATH = "res://Scripts/Calculator/Equation.gd"
 # var a = 2
 # var b = "text"
 
+var sum = -1
+var numbers = 9
+var list = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_equation() 
+	list.append(load_equation())
+	random_numbers() 
 	pass # Replace with function body.
 
 func load_equation():
 	var equation = load(PATH)
 	randomize_numbers()
-	print(get_number_1(), " + ", get_number_2())
-	
+	self.text = str(get_number_1(), " + ", get_number_2())
 	var sum = generate_sum()
+	self.sum = sum
 	self.text = str(sum)
-	pass 
+	return sum
+	
+func random_numbers(numbers = (self.numbers - 1), index = -1):
+	var rng = RandomNumberGenerator.new()
+	# var number_range = self.sum
+	rng.randomize()
+	var list = []
+	for i in range(numbers):
+			list.append(rng.randi_range(0 , 100))
+	list.append(self.sum)
+	print("lista: ", list)
+	list = self.list
+	return list
